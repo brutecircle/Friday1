0307503d8f430416b339e0494b5d28d792585042
Talent M <simeontofiakwa@gmail.com>
2021-11-05 14:14:29 +0000

    java ex2

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// ex1
+let  x = "John";
+let y = "Doe";
+console.log(`"${x} <> ${y}"`);
+ 
+// ex 2 
+// const person = new Object();
+// person.name = 'Talent',
+// person.surname = 'Antonio',
+// person.email = 'antonio@strive.com'
+
+const person = {
+    name: 'Talent',
+    surname: 'Antonio',
+    email:'antonio@strive.com'
+  };
